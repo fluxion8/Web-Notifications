42dd220cca4b1c739f6df83872005069a46f3c08
Onur <onraydeniz2@gmail.com>
2021-11-12 20:47:51 +0300

    fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path');
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
